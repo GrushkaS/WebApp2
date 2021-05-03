897d4f2d3d8ba1ca234e02397202852e70eb1471
Vadim <vadimdrach600@gmail.com>
2021-05-03 17:33:34 +0300

    Initial empty project with some logic on the server
    Added main logic and main components in Asp.Net

---
+++ WebApp2/Controllers/MainController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp2.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp2.Controllers
+{
+    [ApiController]
+    [Route("posts")]
+    public class MainController: Controller
+    {
+        ApplicationContext db;
+        public MainController(ApplicationContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Post> Get()
+        {
+            return db.Posts.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Post Get(int id)
+        {
+            return db.Posts.FirstOrDefault(x => x.Id==id);
+        }
+    }
+}
