c8ce1a546cba686e2614a461dc49cb36f47307a0
Vadim <vadimdrach600@gmail.com>
2021-05-05 13:50:29 +0300

    Added sign-up page
    Added sign-up page front-end

--- WebApp2/ClientApp/src/app/app.module.ts
+++ WebApp2/ClientApp/src/app/app.module.ts
@@ -11,12 +11,14 @@ import { PostListComponent } from './post-list/post-list.component';
 import {PostViewComponent} from './post-view/post-view.component';
 import {NotFoundComponent} from './not-found/not-found.component';
 import {LogInComponent} from './authorization/log-in/log-in.component';
+import {SignUpComponent} from './authorization/sign-up/sign-up.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'posts-list', component: PostListComponent},
   { path: 'posts-list/:id', component: PostViewComponent, pathMatch: 'full'},
   { path: 'log-in', component: LogInComponent},
+  { path: 'sign-up', component: SignUpComponent},
   { path: '**', component: NotFoundComponent}
 ];
 
@@ -29,7 +31,8 @@ const appRoutes: Routes = [
     PostListComponent,
     PostViewComponent,
     NotFoundComponent,
-    LogInComponent
+    LogInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
