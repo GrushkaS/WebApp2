37ad684c9550e21222b1835e67a92fbadf610ea4
Vadim <vadimdrach600@gmail.com>
2021-05-06 17:18:59 +0300

    Completed edit page for admin routes
    Finished crud components for admin

--- WebApp2/ClientApp/src/app/admin/admin-post-edit/admin-post-edit.component.ts
+++ WebApp2/ClientApp/src/app/admin/admin-post-edit/admin-post-edit.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { DataService} from '../../services/data.service';
+import { Post} from '../../models/post';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'admin-app-post-edit',
   templateUrl: './admin-post-edit.component.html',
+  providers: [DataService]
 })
-export class AdminPostEditComponent {
+export class AdminPostEditComponent implements OnInit{
+  id: number;
+  post: Post;
+  constructor(private dataService: DataService, private router: Router, activeRoute: ActivatedRoute) {
+    this.id = Number.parseInt(activeRoute.snapshot.params['id']);
+  }
+
+  ngOnInit() {
+    if (this.id) {
+      this.dataService.getPost(this.id)
+        .subscribe((data: Post) => {
+          this.post = data;
+        });
+    }
+  }
+
+  save() {
+    this.dataService.updatePost(this.post).subscribe(data => this.router.navigateByUrl('/admin/posts-list'));
+  }
+
+
 }
