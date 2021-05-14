a5aac34ae3b58ea4530b866aa278397733072b0f
Vadim <vadimdrach600@gmail.com>
2021-05-14 13:52:45 +0300

    Added recommendations

--- WebApp2/ClientApp/src/app/services/data.analysis.ts
+++ WebApp2/ClientApp/src/app/services/data.analysis.ts
@@ -9,6 +9,10 @@ export class DataAnalysis implements OnInit {
   public counter: Counter;
   public id = 0;
 
+  max1: number;
+  max2: number;
+  maxIdPost: number[];
+
   constructor(private dataService: DataService) {
     this.counters = [];
     this.dataService.getPosts().subscribe((data: Post[]) => {
@@ -19,7 +23,7 @@ export class DataAnalysis implements OnInit {
         this.counter.counter = 0;
         this.counters.push(this.counter);
       });
-
+      console.log('DataAnalysis: ' + this.counters);
     });
   }
 
@@ -42,6 +46,23 @@ export class DataAnalysis implements OnInit {
       }
       console.log('Counter: ' + element.idPost + ' : ' + element.counter);
     });
+  }
+
+  getMostViewed() {
+    this.maxIdPost = [];
+    this.maxIdPost.push(this.counters[0].idPost);
+    this.maxIdPost.push(this.counters[1].idPost);
+    this.max1 = this.counters[0].counter;
+    this.max2 = this.counters[1].counter;
+    this.counters.forEach(element => {
+      if (element.counter > this.max1) {
+        this.max2 = this.max1;
+        this.maxIdPost[1] = this.maxIdPost[0];
+        this.max1 = element.counter;
+        this.maxIdPost[0] = element.idPost;
+      }
+    });
+    return this.maxIdPost;
   }
 
   updateCounters() {
--- WebApp2/ClientApp/src/app/unlogined/recommendation/recommendation.component.ts
+++ WebApp2/ClientApp/src/app/unlogined/recommendation/recommendation.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DataAnalysis} from '../../services/data.analysis';
+import {DataService} from '../../services/data.service';
+import {Post} from '../../models/post';
 
 @Component({
   selector: 'app-home-rec',
   templateUrl: './recommendation.component.html',
   styleUrls: ['./recommendation.component.css']
 })
-export class RecommendationComponent {
-  constructor(dataAnalysis: DataAnalysis) {
+export class RecommendationComponent implements OnInit {
+  Posts: Post[];
+
+  maxIdPost: number[];
+  isEmpty = true;
+
+  constructor(private dataAnalysis: DataAnalysis, private dataService: DataService) {
+  }
+  ngOnInit() {
+    this.maxIdPost = this.dataAnalysis.getMostViewed();
+    this.Posts = [];
+    this.dataService.getPost(this.maxIdPost[0]).subscribe((data: Post) => {this.Posts.push(data); } );
+    this.dataService.getPost(this.maxIdPost[1]).subscribe((data: Post) => {this.Posts.push(data); } );
+    this.isEmpty = false;
   }
 }
